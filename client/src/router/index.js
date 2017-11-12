03257a3f83a952501c32b7c92b5c0b70b69b6ef0
ronny torres <ronnytorresmtz@gmail.com>
2017-11-12 13:37:40 -0600

    add path for all path does not found

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
 import Login from '@/pages/Login';
 
 Vue.use(Router);
@@ -9,13 +8,12 @@ export default new Router({
   mode: 'history',
   routes: [
     {
-      path: '/',
-      name: 'Hello',
-      component: HelloWorld,
+      path: '/Login',
+      name: 'Login',
+      component: Login,
     },
     {
-      path: '/',
-      name: 'Login',
+      path: '*',
       component: Login,
     },
   ],
