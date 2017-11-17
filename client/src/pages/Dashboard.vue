644a168714dd37be7cb7552ac3b1a1ec68427f2b
ronny torres <ronnytorresmtz@gmail.com>
2017-11-16 21:29:12 -0600

    first commit

---
+++ client/src/pages/Dashboard.vue
@@ -0,0 +1,37 @@
+<style scoped>
+
+.title {
+  padding: 100px;
+}
+ 
+</style>
+
+<template>
+  <div>
+    <topbar></topbar>
+    <div class="title"> 
+      <h1>You have permissions to access the application </h1>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  import topbar from '../components/layout/topbar';
+
+  export default {
+  
+    components: {
+      topbar,
+    },
+
+    data() {
+      return {
+      };
+    },
+
+    methods: {
+
+    },
+  };
+</script>
