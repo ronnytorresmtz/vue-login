69b9341e9b4d09dcd2d590488723ae24b9cc10c8
ronny torres <ronnytorresmtz@gmail.com>
2017-11-16 21:35:23 -0600

    add 2 users demo123 y ronnytorresmtz

--- server/database/seeds/DatabaseSeeder.php
+++ server/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use MyCode\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +12,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\User::class, 10)->create();
+        factory(\MyCode\Models\User::class, 10)->create();
+
+        User::Create([
+            'name' => 'demo123',
+            'email' => 'demo123@gmail.com',
+            'password' => bcrypt('demo123'),
+            'remember_token' => str_random(10),
+        ]);
+
+        User::Create([
+            'name' => 'ronnytorresmtz',
+            'email' => 'ronnytorresmtz@gmail.com',
+            'password' => bcrypt('ronnytorresmtz'),
+            'remember_token' => str_random(10),
+        ]);
     }
 }
