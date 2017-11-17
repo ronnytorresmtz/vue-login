770b601f80a50a655d3e07b6f4fb7640b6792150
ronny torres <ronnytorresmtz@gmail.com>
2017-11-16 21:20:13 -0600

    Add the topbar component

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,20 +9,20 @@ body {
 
 <template>
   <div id="app" style="margin:0px;">
-    <topbar></topbar>
+    
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 
-import topbar from './components/layout/topbar';
+// import topbar from './components/layout/topbar';
 
 export default {
   name: 'app',
 
-  components: {
-    topbar,
-  },
+  // components: {
+  //   topbar,
+  // },
 };
 </script>
