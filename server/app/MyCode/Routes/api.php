74a8a29a8cb5c68221db450ef950311062fb4616
ronny torres <ronnytorresmtz@gmail.com>
2017-11-21 20:32:08 -0600

    add a route with middleware to return some json text

--- server/app/MyCode/Routes/api.php
+++ server/app/MyCode/Routes/api.php
@@ -1,5 +1,12 @@
 <?php
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+use MyCode\Models\User as User;
+
+Route::middleware('auth:api')->get('/users', function () {
+
+    return response()->json([
+        'message' => 'Communication is established with Laravel API via Laravel Passport and CORS',
+        'middleware' => 'Laravel routes are protected with a middleware guard - auth:api',
+        'users' => User::all(),
+    ]);
 });
