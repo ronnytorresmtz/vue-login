8d81b2a491334f6eaf832a8c93b74f90dbe3ad5f
ronny torres <ronnytorresmtz@gmail.com>
2017-11-21 20:30:05 -0600

    add axios interceptor

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,21 @@
 import Vue from 'vue';
+import Axios from 'axios';
 import App from './App';
 import router from './router';
 import store from './store/Store';
+import Auth from './lib/Auth';
 
 Vue.config.productionTip = false;
+
+Axios.interceptors.request.use((config) => {
+  const request = config;
+  if (Auth.getToken() !== null) {
+    request.headers.accept = 'application/json, text/plain, */*';
+    request.headers.authorization = Auth.getAuthHeader();
+  }
+  return request;
+}, error => console.log(error));
+
 
 /* eslint-disable no-new */
 new Vue({
