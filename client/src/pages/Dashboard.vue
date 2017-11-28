ec83116503e7d73526fda6030d5fad273dbfe006
ronny torres <ronnytorresmtz@gmail.com>
2017-11-27 18:49:02 -0600

    add a navbar

--- client/src/pages/Dashboard.vue
+++ client/src/pages/Dashboard.vue
@@ -21,11 +21,47 @@
   margin-top: 30px;
   padding: 30px;
 }
+.spin {
+  font-size: 2em;
+}
+
+.spin-centered {
+  position: absolute;
+  top: 45%;
+  left: 45%;
+}
+ .brand {
+    text-align: left !important;
+    font-size: 22px;
+    padding-left: 20px;
+    padding-right: 50px;
+    line-height: 50px !important;
+    /*background-color: #337ab7;*/
+    line-height: 50px;
+    display: block;
+    text-align: center;
+    color: #e1ffff;
+    font-family: verdana;
+  }
+
 </style>
 
 <template>
   <div>
-    <topbar></topbar>
+    <h3 class="spin spin-centered" v-if="!info">
+      <i class="fa fa-spinner fa-spin" aria-hidden="true"></i> Loading...
+    </h3>
+
+     <!--blue topbar-->
+    <nav class="navbar navbar-default navbar-fixed-top" style="background:#3c8dbc" >
+    <div class="brand">
+      Brandname
+    </div>
+    </nav>
+    <!--End blue topbar-->
+
+    <topbar v-if="info"></topbar>
+
     <div class="title" v-if="info"> 
       <h2 class="message">{{ info.message }}</h2>
       <h4 class="message message-below">{{ info.middleware }}</h4>
@@ -34,6 +70,7 @@
       stored in the localstorge. After that, for each request the application send the access token stored in the request header 
       and a laravel middleware allows or not to access the API. 
       </p>
+      <h3> Current User: {{ info.user.email }} </h3>
       <hr>
       <h2>User List (laravel route: '/users')</i></h2>
       <table class="table table-hover table-bordered table-striped">
