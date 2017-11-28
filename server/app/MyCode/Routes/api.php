04baa160f602ba44a1e586d25a821cbd491f55f8
ronny torres <ronnytorresmtz@gmail.com>
2017-11-27 22:48:45 -0600

    delete the logout route

--- server/app/MyCode/Routes/api.php
+++ server/app/MyCode/Routes/api.php
@@ -5,8 +5,16 @@ use MyCode\Models\User as User;
 Route::middleware('auth:api')->get('/users', function () {
 
     return response()->json([
+
         'message' => 'Communication is established with Laravel API via Laravel Passport and CORS',
+
         'middleware' => 'Laravel routes are protected with a middleware guard - auth:api',
+
         'users' => User::all(),
+
+        'user' => Request::user(),
+
     ]);
+
 });
+
