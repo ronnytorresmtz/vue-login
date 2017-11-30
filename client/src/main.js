3132ee01d866d87a18762ff477303899cd34ffb1
ronny torres <ronnytorresmtz@gmail.com>
2017-11-30 00:03:32 -0600

    add a require of es6-promise'

--- client/src/main.js
+++ client/src/main.js
@@ -1,10 +1,11 @@
-// require('es6-promise').polyfill();
 import Vue from 'vue';
 import Axios from 'axios';
 import App from './App';
 import router from './router';
 import store from './store/Store';
 import Auth from './lib/Auth';
+
+require('es6-promise').polyfill();
 
 Vue.config.productionTip = false;
 
